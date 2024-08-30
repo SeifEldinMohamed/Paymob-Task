c474a784c31cdab90d216bbfc6935c1b3aeb4254
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2024-08-30 15:57:54 +0300

    create popular movies screen

---
+++ app/src/main/java/com/example/paymobtaskmoviesapp/presentation/screens/movies_home_screen/PopularMoviesUiState.kt
@@ -0,0 +1,14 @@
+package com.example.paymobtaskmoviesapp.presentation.screens.movies_home_screen
+
+import androidx.paging.PagingData
+import com.example.paymobtaskmoviesapp.presentation.screens.movies_home_screen.model.CustomExceptionUiModel
+import com.example.paymobtaskmoviesapp.presentation.screens.movies_home_screen.model.PopularMovieUiModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+data class PopularMoviesUiState(
+    val popularMoviesPagingDataFlow: Flow<PagingData<PopularMovieUiModel>> = emptyFlow(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val customErrorExceptionUiModel: CustomExceptionUiModel = CustomExceptionUiModel.Unknown
+)
